f46059e7cbe3682d06a763288f0f99ac68cd7ce8
michaeltaman <42732094+michaeltaman@users.noreply.github.com>
2021-09-06 13:02:44 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(item, idx) in items" :key="idx">
+        {{ item }}
+      </li>
+    </ul>
+    <button @click="addAndUpdate">Add and update</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { name: "Alpha", price: 1 },
+        { name: "Beta", price: 2 },
+      ],
+    };
+  },
+  methods: {
+    addAndUpdate() {
+      console.log(this.items);
+      const newItem = { name: "New", price: "-" };
+      this.items.push(newItem);
+
+      console.log("here", this.items[this.items.length - 1] === newItem);
+      setTimeout(() => {
+        newItem.price = "over 9000";
+      }, 3000);
+    },
+  },
+};
+</script>
